269412ffa941ed3f07a1028c334ae90ed2f167f6
aleaplicativos <122333431+aleaplicativos@users.noreply.github.com>
2023-10-05 22:36:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -237,6 +237,7 @@ const games = [
     {link: "https://aleaplicativos.github.io/Physics-Playground/"},
     {link: "https://aleaplicativos.github.io/Noisy-ghost/"},
     {link: "https://aleaplicativos.github.io/batcat/"},
+    {link: "https://aleaplicativos.github.io/Skull-Toggle/"},
     {      
       link: "https://aleaplicativos.github.io/bolamagnetica/"
     },
