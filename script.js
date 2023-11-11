be7d684207d6330fd9b94d27d689d7b95f0d8fa5
aleaplicativos <122333431+aleaplicativos@users.noreply.github.com>
2023-11-11 16:27:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -259,6 +259,7 @@ const games = [
     {link: "https://aleaplicativos.github.io/encontre-o-emoji/"},
     {link: "https://aleaplicativos.github.io/dragao-colorido/"},
      {link: "https://aleaplicativos.github.io/peixe-3d/"},
+     {link: "https://aleaplicativos.github.io/emojiresponsive/"},
      {link: "https://aleaplicativos.github.io/bolamagnetica/"},
     // Adicione quantos jogos desejar nesta lista
   ];
