22af311aa20d0f4eadea0fa5cfbf62eb4e514a53
aleaplicativos <122333431+aleaplicativos@users.noreply.github.com>
2023-12-19 11:24:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -270,6 +270,7 @@ const games = [
     {link: "https://aleaplicativos.github.io/30.000-particles/"},
     {link: "https://aleaplicativos.github.io/Creepy-Crawly/"},
     {link: "https://aleaplicativos.github.io/the-line-follows/"},
+    {link: "https://aleaplicativos.github.io/help-papai-noel/"},
      {link: "https://aleaplicativos.github.io/bolamagnetica/"},
     // Adicione quantos jogos desejar nesta lista
   ];
