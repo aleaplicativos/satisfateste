bec649f106ca4e66c091526ec1b9604636c02f00
aleaplicativos <122333431+aleaplicativos@users.noreply.github.com>
2024-04-03 10:10:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -137,6 +137,7 @@ const games = [
     {link: "https://aleaplicativos.github.io/urso-curiso/"},
     {link: "https://aleaplicativos.github.io/ursinhocolor/"},
     {link: "https://aleaplicativos.github.io/estrelamagica/"},
+    {link: "https://aleaplicativos.github.io/bubles/"},
     {link: "https://aleaplicativos.github.io/bolamagnetica/"},
     // Adicione quantos jogos desejar nesta lista
   ];
